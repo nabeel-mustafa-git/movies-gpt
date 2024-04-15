feb85d75a4f60a6729b6cee56f9adbc6f4fe7bc1
Nabeel Mustafa <nabeelmustafaskp@gmail.com>
2024-04-15 22:35:41 +0500

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,32 +13,36 @@ const Body = () => {
     {
       path: "/",
       element: <Login />,
+      errorElement: <Error />,
     },
     {
       path: "/signup",
       element: <Login login={false} />,
+      errorElement: <Error />,
     },
     {
       path: "/browse",
       element: <Browse />,
+      errorElement: <Error />,
     },
     {
       path: "/suggestion",
       element: <Suggestion />,
+      errorElement: <Error />,
     },
     {
       path: "/search",
       element: <Search />,
+      errorElement: <Error />,
     },
     {
       path: "/movie/:id",
       element: <MovieDetail />,
+      errorElement: <Error />,
     },
     {
       path: "*",
       element: <PageNotFound />,
-    },
-    {
       errorElement: <Error />,
     },
   ]);
